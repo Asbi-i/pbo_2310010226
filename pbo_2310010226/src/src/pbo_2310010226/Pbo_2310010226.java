b4f813378cc5348c0a48ccfa6d624465ab531f0d
Ahmad Anshori Azhbi <140075392+Asbi-i@users.noreply.github.com>
2026-01-12 22:16:30 +0700

    Add files via upload

---
+++ pbo_2310010226/src/src/pbo_2310010226/Pbo_2310010226.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pbo_2310010226;
+import form.*;
+/**
+ *
+ * @author ACER
+ */
+public class Pbo_2310010226 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new frameLogin().setVisible(true);
+
+    }
+    
+}
